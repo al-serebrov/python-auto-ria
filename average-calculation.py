4edeee6008b350d863f92be2f932de9b54f47143
Aleksandr Serebrov <sera@ciklum.com>
2017-04-12 17:48:06 +0300

    changes in composing parameters for GET request

--- average-calculation.py
+++ average-calculation.py
@@ -17,35 +17,36 @@ VEHICLE_MODEL = 'Focus'
 VEHICLE_BODY = None
 VEHICLE_YEAR_START = '2000'
 VEHICLE_YEAR_END = '2006'
-VEHICLE_MIN_PRICE = '4000'
-VEHICLE_MAX_PRICE = '5000'
 VEHICLE_REGION = 'Харьков'
+# not implemented yet
+# VEHICLE_MIN_PRICE = '4000'
+# VEHICLE_MAX_PRICE = '5000'
 # end
 
-# Human readable implementation of auto.ria.com API parameters
+# auto.ria.com API parameters, defaulted to None
 PARAMS = {
-    'category': 'main_category',
-    'body': 'body_id',
-    'mark': 'marka_id',
-    'model': 'model_id',
-    'years': 'yers',
-    'gear_type': 'gear_id',
-    'fuel': 'fuel_id',
-    'drive': 'drive_id',
-    'engine': 'engineVolume',
-    'options': 'options',
-    'mileage': 'raceInt',
-    'doors': 'door',
-    'state': 'state_id',
-    'city': 'city_id',
-    'carrying': 'carrying',
-    'seats': 'seats',
-    'color': 'color_id',
-    'custom': 'custom',
-    'damage': 'damage',
-    'credit': 'under_credit',
-    'confiscated': 'confiscated_car',
-    'on_repairment': 'onRepairParts',
+    'main_category': None,
+    'body_id': None,
+    'marka_id': None,
+    'model_id': None,
+    'yers': None,
+    'gear_id': None,
+    'fuel_id': None,
+    'drive_id': None,
+    'engineVolume': None,
+    'options': None,
+    'raceInt': None,
+    'door': None,
+    'state_id': None,
+    'city_id': None,
+    'carrying': None,
+    'seats': None,
+    'color_id': None,
+    'custom': None,
+    'damage': None,
+    'under_credit': None,
+    'confiscated_car': None,
+    'onRepairParts': None,
 }
 
 
@@ -65,21 +66,21 @@ def get_types():
 def get_bodystyles(category):
     """Get available bodystyles from auto.ria.com."""
     url = 'http://api.auto.ria.com/categories/{}/bodystyles' \
-          .format(str(category['value']))
+          .format(str(category))
     return jsonify_response(url)
 
 
 def get_marks(category):
     """Get available car marks from auto.ria.com."""
     url = 'http://api.auto.ria.com/categories/{}/marks' \
-          .format(str(category['value']))
+          .format(str(category))
     return jsonify_response(url)
 
 
 def get_models(category, mark):
     """Get available models for selected mark from auto.ria.com."""
     url = 'http://api.auto.ria.com/categories/{}/marks/{}/models' \
-          .format(str(category['value']), str(mark['value']))
+          .format(str(category), str(mark))
     return jsonify_response(url)
 
 
@@ -93,7 +94,7 @@ def select_type(vehicle_type):
     """Select vehicle type from available types."""
     for ve_type in types_list:
         if str(ve_type['name']) == vehicle_type:
-            return ve_type
+            return ve_type['value']
 
 
 def select_bodystyle(bodystyle):
@@ -101,28 +102,28 @@ def select_bodystyle(bodystyle):
     if bodystyle is not None:
         for ve_bstyle in bodystyles_list:
             if str(ve_bstyle['name']) == bodystyle:
-                return ve_bstyle
+                return ve_bstyle['value']
 
 
 def select_mark(mark):
     """Select mark from available marks."""
     for ve_mark in marks_list:
         if str(ve_mark['name']) == mark:
-            return ve_mark
+            return ve_mark['value']
 
 
 def select_model(model):
     """Select model from available models."""
     for ve_model in models_list:
         if str(ve_model['name']) == model:
-            return ve_model
+            return ve_model['value']
 
 
 def select_state(state):
     """Select states from available states."""
     for ve_state in states_list:
         if fnmatch(ve_state['name'], '*' + state + '*'):
-            return ve_state
+            return ve_state['value']
 
 
 def compose_parameters():
@@ -130,9 +131,9 @@ def compose_parameters():
     params_dict = {}
     years_list = []
     global PARAMS
-    params_dict[PARAMS['category']] = category_auto['value']
-    params_dict[PARAMS['mark']] = mark['value']
-    params_dict[PARAMS['model']] = model['value']
+    params_dict['main_category'] = category_auto
+    params_dict['marka_id'] = mark
+    params_dict['model_id'] = model
     if VEHICLE_YEAR_START is not None and VEHICLE_YEAR_END is not None:
         years_list.append(VEHICLE_YEAR_START)
         years_list.append(VEHICLE_YEAR_END)
@@ -140,11 +141,11 @@ def compose_parameters():
         years_list.append(VEHICLE_YEAR_END)
     else:
         years_list.append(VEHICLE_YEAR_START)
-    params_dict[PARAMS['years']] = years_list
+    params_dict['yers'] = years_list
     if VEHICLE_BODY is not None:
-        params_dict[PARAMS['body']] = bodystyle
+        params_dict['body_id'] = bodystyle
     if VEHICLE_REGION is not None:
-        params_dict[PARAMS['state']] = state['value']
+        params_dict['state_id'] = state
     return params_dict
 
 """
