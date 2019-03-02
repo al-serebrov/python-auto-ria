25b045d283a87fface3f338e970db865d117d64d
Alexander Serebrov <serebrov.alexandr@gmail.com>
2019-03-02 17:31:34 +0200

    Fix usage example according to the code review.

--- average_calculation.py
+++ average_calculation.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """Python auto.ria.com API.
 
 Python implementation of API intended for calulating
@@ -14,8 +16,11 @@ print(api.get_fuels())
 print(api.get_colors())
 """
 
+from math import ceil
 from pprint import pprint
+
 from autoria.api import RiaAPI, RiaAverageCarPrice
+
 # not implemented yet
 # VEHICLE_MIN_PRICE = '4000'
 # VEHICLE_MAX_PRICE = '5000'
@@ -26,16 +31,16 @@ from autoria.api import RiaAPI, RiaAverageCarPrice
 api = RiaAPI()
 search_params = {
     'category': 'Легковые',
-    'mark': 'Mitsubishi',
-    'model': 'ASX',
-    # 'bodystyle': '',
+    'mark': 'Mazda',
+    'model': 'CX-5',
+    'bodystyle': 'Внедорожник / Кроссовер',
     'years': [2015, 2017],
     # 'state': 'Винницкая',
     # 'city': 'Винница',
     'gears': ['Автомат'],
     # 'opts': ['ABS'],
     # 'mileage': [10, 200],
-    # 'fuels': ['Дизель'],
+    'fuels': ['Дизель'],
     # 'drives': ['Полный'],
     # 'color': 'Серый',
     # 'engine_volume': 1.5,
@@ -48,30 +53,8 @@ search_params = {
     'confiscated': False,
     'on_repair_parts': False
 }
-myCarAveragePrice = RiaAverageCarPrice(
-    category=search_params.get('category'),
-    mark=search_params.get('mark'),
-    model=search_params.get('model'),
-    bodystyle=search_params.get('bodystyle'),
-    years=search_params.get('years'),
-    state=search_params.get('state'),
-    city=search_params.get('city'),
-    gears=search_params.get('gears'),
-    opts=search_params.get('opts'),
-    mileage=search_params.get('mileage'),
-    fuels=search_params.get('fuels'),
-    drives=search_params.get('drives'),
-    color=search_params.get('color'),
-    engine_volume=search_params.get('engine_volume'),
-    seats=search_params.get('seats'),
-    doors=search_params.get('doors'),
-    carrying=search_params.get('carrying'),
-    custom=search_params.get('custom'),
-    damage=search_params.get('damage'),
-    under_credit=search_params.get('under_credit'),
-    confiscated=search_params.get('confiscated'),
-    on_repair_parts=search_params.get('on_repair_parts')
-)
+
+myCarAveragePrice = RiaAverageCarPrice(**search_params)
 
 base_url = "https://auto.ria.com/auto__{}.html"
 average = myCarAveragePrice.get_average()
@@ -80,7 +63,8 @@ report = {
     'search_params': search_params,
     'average': average,
     'classifieds_urls': [
-        base_url.format(item)
-        for item in average.get('classifieds')]
+        ['${}'.format(ceil(price)), base_url.format(item)]
+        for price, item in zip(
+            average.get('prices'), average.get('classifieds'))]
 }
 pprint(report)
